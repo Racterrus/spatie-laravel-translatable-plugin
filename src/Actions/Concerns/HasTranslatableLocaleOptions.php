d57578a81e9f1ce690ba00836cdfa6b084625137
Racterrus <antonioracter@yandex.ru>
2024-04-06 20:04:29 +0300

    Update HasTranslatableLocaleOptions.php
    Теперь ключик в массиве $locales - это код локали, а значение - расшифровка (для списка в админке)

--- src/Actions/Concerns/HasTranslatableLocaleOptions.php
+++ src/Actions/Concerns/HasTranslatableLocaleOptions.php
@@ -20,8 +20,12 @@ trait HasTranslatableLocaleOptions
             /** @var SpatieLaravelTranslatablePlugin $plugin */
             $plugin = filament('spatie-laravel-translatable');
 
-            foreach ($livewire->getTranslatableLocales() as $locale) {
-                $locales[$locale] = $plugin->getLocaleLabel($locale) ?? $locale;
+            // foreach ($livewire->getTranslatableLocales() as $locale) { // оригинальная строка
+            foreach ($livewire->getTranslatableLocales() as $key => $locale) { // Изменено Racter. Перебор исходного массива с языками, где ключик является расшифровкой локали, а значение - это локаль.
+                $locales[$locale] = $plugin->getLocaleLabel($locale) ?? $locale; // Оригинальная строка, вместо конструкции с if. Создание массива языков, где ключик - язык, а значение - его полное название, метод плагин getLocaleLabel(); //- переводит ключик (код языка) в полное название языка на языке сайта, если заменить его на null - в значении используются только сокращения
+                if (is_string($key)) { // Добавлено Racter. Если ключик - это строка...
+                    $locales[$locale] = $key;
+                }
             }
 
             return $locales;
